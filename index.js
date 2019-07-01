89725f519a66cccaf270e5a6a67eb5bc54b0957a
Adam Fedor <blindmouse@Adams-MacBook-Air.local>
2019-06-30 17:06:15 -0700

    Corrected import of CSV for header row and added skipLines.

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const results = {};
 
 let getCSV = (cb) => {
     fs.createReadStream('data.csv')
-        .pipe(csv(['WCAG',1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]))
+        .pipe(csv({
+            skipLines: 6,
+            headers: ['WCAG', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+        }))
         .on('data', (data) => {
             results[data['WCAG']] = []
             for (key in data) {
